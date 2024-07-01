0a5d737b06d462760c7cee01595a8033840cf02f
ace-cltr <anand.vish201@gmail.com>
2024-07-01 21:53:14 +0530

    made navbar responsive

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,19 +1,19 @@
-
-// import { CopyBlock, dracula } from "react-code-blocks";
 import { Link } from "react-router-dom";
 
-const LandingPage= () => {
+const LandingPage = () => {
   return (
-    <div className="bg-rose-50 flex flex-row justify-center items-center min-h-screen">
-      <div className="bg-white shadow-lg flex flex-col  rounded-2xl w-[90vw] h-[90vh]">
-        <div className="shadow-lg flex justify-between items-center border-2 border-amber-500 rounded-2xl px-12 py-2 m-4 mb-7">
+    <div className="bg-rose-50 flex flex-col items-center min-h-screen p-4 md:flex-row md:justify-center">
+      <div className="bg-white shadow-lg flex flex-col rounded-2xl w-full md:w-[90vw] h-full md:h-[90vh]">
+        <div className="shadow-lg flex flex-col md:flex-row justify-between items-center border-2 border-amber-500 rounded-2xl p-4 m-4 mb-7">
           <Link to="*">
-            <h1 className="font-[pacifico] text-amber-400 flex flex-row justify-center items-center text-4xl m-2 cursor-pointer">
+            <h1 className="font-[pacifico] text-amber-400 text-4xl m-2 cursor-pointer">
               Payments
             </h1>
           </Link>
-          <div className="flex items-center space-x-4">
-            <p className="font-mono text-l">New user? first</p>
+          <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 mt-4 md:mt-0">
+            <p className="font-mono text-center md:text-left">
+              New user? first
+            </p>
             <Link to="/signup">
               <button className="font-mono shadow-md border-2 border-black bg-amber-400 p-1 rounded-md">
                 Sign up
@@ -26,12 +26,12 @@ const LandingPage= () => {
             </Link>
           </div>
         </div>
-        <p className="font-mono mx-5">
-          Hi there, this Website focuses on how payments work behind the seen in
-          the Backend.
+        <p className="font-mono mx-5 text-center md:text-left">
+          Hi there, this Website focuses on how payments work behind the scenes
+          in the Backend.
           <br /> You might have wondered what will happen if I send two people
-          some money at the same time and break the system... wha-t you haven't
-          too bad I'm still gonna tell you why this doesn't happens (usually),
+          some money at the same time and break the system... what! you haven't
+          too bad I'm still gonna tell you why this doesn't happen (usually).
           There is a concept called{" "}
           <strong className="text-amber-600 text-xl">Sessions</strong>, <br />{" "}
           What is a Session anyways? and what makes it so useful in the
