35c2f5ed25c6b8b3090ee5260c12c8ceb9e751cb
ace-cltr <anand.vish201@gmail.com>
2024-07-01 21:58:57 +0530

    responsive edits

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="bg-rose-50 flex flex-row justify-center items-center min-h-screen">
-      <div className="bg-rose-50 shadow-lg flex flex-col border-2 border-amber-300 rounded-2xl px-24 py-4">
+    <div className="bg-rose-50 flex flex-col justify-center items-center min-h-screen md:flex-row">
+      <div className="bg-rose-50 shadow-lg flex flex-col border-2 border-amber-300 rounded-2xl px-8 py-4 sm:px-12 md:px-24">
         <BrandLogo>Payments</BrandLogo>
         <Heading>Sign Up</Heading>
         <InputGroupSignUp />
